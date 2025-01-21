7a640e4e31fdf4bdf7debfd52ec348ee88fab423
Michael Jiang <mjiang@lcs.on.ca>
2025-01-21 11:25:00 -0500

    Initial Commit

---
+++ TimeConverterApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeConverterApp
+//
+//  Created by Michael on 2025/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeConverterApp/TimeConverterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConverterAppApp.swift
+//  TimeConverterApp
+//
+//  Created by Michael on 2025/1/21.
+//
+
+import SwiftUI
+
+@main
+struct TimeConverterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
