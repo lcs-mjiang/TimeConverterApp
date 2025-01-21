c15953f11a9c23487c44d3d57eff26bc9fbb00f1
Michael Jiang <mjiang@lcs.on.ca>
2025-01-21 11:36:33 -0500

    Create a button let the converter work.

--- TimeConverterApp/TimeConverterView.swift
+++ TimeConverterApp/TimeConverterView.swift
@@ -12,12 +12,19 @@ struct TimeConverterView: View {
     @State private var minutes: Int? = nil
     @State private var seconds: Int? = nil
     @State private var feedback: String = ""
+   
     var body: some View {
         NavigationStack {
-            VStack {TextField("Enter hours", text: $hoursInput)
+            VStack(spacing: 30) {TextField("Enter hours", text: $hoursInput)
                     .textFieldStyle(.roundedBorder)
                     .keyboardType(.decimalPad)
             }
+            Button {
+            } label: {
+                Text("Convert")
+            }
+            .buttonStyle(.borderedProminent)
+
             .navigationTitle("Time Converter")
         }
     }
